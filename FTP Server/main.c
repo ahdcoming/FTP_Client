9968a200cd0cac3feb6c910239af03a0a2daa3b2
tyler@mbp <ouyang@cs.ucsb.edu>
2016-01-30 23:13:43 -0800

    Initial Commit

---
+++ FTP Server/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  FTP Server
+//
+//  Created by Tyler Weimin Ouyang on 1/30/16.
+//  Copyright © 2016 Tyler Ouyang. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  printf("Hello, World!\n");
+    return 0;
+}
