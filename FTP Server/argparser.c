396e66866ef6ed862c679a1f1f9ea0e88c26eee8
tyler@mbp <ouyang@cs.ucsb.edu>
2016-01-31 20:14:54 -0800

    arguments parser

---
+++ FTP Server/argparser.c
@@ -0,0 +1,81 @@
+//
+//  argparser.c
+//  FTP Server
+//
+//  Created by Tyler Weimin Ouyang on 1/31/16.
+//  Copyright © 2016 Tyler Ouyang. All rights reserved.
+//
+
+#include <stdlib.h>
+#include <string.h>
+#include "argparser.h"
+#include "options.h"
+
+/* Parse a single option. */
+error_t parse_opt (int key, char *arg, struct argp_state *state) {
+  /* Get the input argument from argp_parse, which we
+   know is a pointer to our arguments structure. */
+  struct arguments *arguments = state->input;
+  
+  switch (key)
+  {
+    case 'v':
+      printf("%s\n", program_version);
+      exit(0);
+      break;
+    case 'h':
+      argp_state_help(state, stdout, ARGP_HELP_STD_HELP);
+      exit(0);
+      break;
+    case 'a':
+      arguments->isAvtive = 1;
+      break;
+    case 'f':
+      arguments->file = arg;
+      break;
+    case 's':
+      arguments->hostname = arg;
+      break;
+    case 'p':
+      arguments->port = atoi(arg);
+      break;
+    case 'n':
+      arguments->user = arg;
+      break;
+    case 'P':
+      arguments->password = arg;
+      break;
+    case 'm':
+      if (strcmp(arg, "ASCII") == 0) {
+        arguments->mode = MODE_ASCII;
+      }
+      else if (strcmp(arg, "binary") == 0) {
+        arguments->mode = MODE_BINARY;
+      }
+      else {
+        argp_usage(state);
+      }
+      break;
+    case 'b':
+      arguments->num_bytes = atoi(arg);
+      break;
+    case 'l':
+      arguments->logIsOn = 1;
+      arguments->logfile = arg;
+      break;
+    case 'w':
+      arguments->swarmIsOn = 1;
+      arguments->swarm_config_file = arg;
+      break;
+    case ARGP_KEY_END:
+      if (state->argc < 2) {
+        /* No inputs. */
+        argp_state_help(state, stdout, ARGP_HELP_STD_HELP);
+        exit(0);
+      }
+      break;
+    default:
+      return ARGP_ERR_UNKNOWN;
+  }
+  return 0;
+}
--- FTP Server/main.c
+++ FTP Server/main.c
@@ -6,10 +6,40 @@
 //  Copyright © 2016 Tyler Ouyang. All rights reserved.
 //
 
+#include <stdlib.h>
 #include <stdio.h>
+#include <argp.h>
 
-int main(int argc, const char * argv[]) {
-  // insert code here...
-  printf("Hello, World!\n");
-    return 0;
+#include "options.h"
+#include "argparser.h"
+
+int main (int argc, char **argv) {
+  struct arguments arguments;
+  
+  /* Default values. */
+  arguments.file = NULL;
+  arguments.hostname = NULL;
+  arguments.user = "anonymous";
+  arguments.password = NULL;
+  arguments.logfile = NULL;
+  arguments.swarm_config_file = NULL;
+  arguments.port = 21;
+  arguments.mode = MODE_BINARY;
+  arguments.isAvtive = 0;
+  arguments.logIsOn = 0;
+  arguments.swarmIsOn = 0;
+  arguments.num_bytes = 0;
+  
+  /* Parse our arguments; every option seen by parse_opt will
+   be reflected in arguments. */
+  argp_parse (&argp, argc, argv, 0, 0, &arguments);
+  
+  if (!arguments.file || !arguments.hostname) {
+    if (!arguments.swarmIsOn) {
+      printf("--file and --server are required.\n");
+      exit(100);
+    }
+  }
+  
+  exit (0);
 }
